acc478826d06b0c51fd27a8ef6f89052ba7b6369
asrulabdullah99 <srulabdullah@gmail.com>
2022-12-07 23:10:57 +0700

    push crud dosen api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\DosenController;
 use App\Http\Controllers\API\MahasiswaController;
 
 Route::controller(MahasiswaController::class)->group(function () {
@@ -9,4 +10,11 @@ Route::controller(MahasiswaController::class)->group(function () {
     Route::post('/mahasiswa/simpan', 'simpan');
     Route::patch('/mahasiswa/{mahasiswa}/update', 'update');
     Route::delete('/mahasiswa/{mahasiswa}/hapus', 'hapus');
+});
+
+Route::controller(DosenController::class)->group(function () {
+    Route::get('/dosen', 'index');
+    Route::post('/dosen/simpan', 'simpan');
+    Route::patch('/dosen/{dosen}/update', 'update');
+    Route::delete('/dosen/{dosen}/hapus', 'hapus');
 });
